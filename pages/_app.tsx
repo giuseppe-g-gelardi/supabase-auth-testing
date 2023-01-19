035a3f55fc3a5639f498c30703c8025694e7f223
n-rdy <giuseppegelardi@icloud.com>
2023-01-19 10:52:30 -0500

    base

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,3 +4,9 @@ import type { AppProps } from 'next/app'
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
+
+
+
+
+// import { Inter } from '@next/font/google'
+// const inter = Inter({ subsets: ['latin'] })
