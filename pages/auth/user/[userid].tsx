ca81a638d06b7abc31fa4e2a89d9e7ff95b69caf
n-rdy <giuseppegelardi@icloud.com>
2023-01-23 08:55:16 -0500

    start outline singple post page

--- pages/auth/user/[userid].tsx
+++ pages/auth/user/[userid].tsx
@@ -3,6 +3,7 @@ import { createServerSupabaseClient } from '@supabase/auth-helpers-nextjs'
 import Avatar from "@/components/Avatar"
 
 import { Database, Profile, Post } from '@/types'
+import Link from "next/link"
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const supabase = createServerSupabaseClient<Database>(ctx)
@@ -13,7 +14,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
   } = await supabase.from('profiles').select('*').eq('id', userid).single()
   const {
     data: posts, error: postError
-  } = await supabase.from('posts').select('*').eq('posted_by', userid)
+  } = await supabase.from('posts').select('*').eq('posted_by', userid).order('created_at', { ascending: false })
 
   if (profileError) console.error(profileError)
   if (postError) console.error(postError)
@@ -30,21 +31,24 @@ export default function UserPage({ profile, posts }: UserPageProps) {
 
 
   return (
-    <div className="flex flex-col gap-5 items-center justify-center bg-gradient-to-r from-indigo-500 h-screen">
+    <div className="flex flex-col gap-5 items-center justify-center bg-gradient-to-r from-indigo-500 h-full">
       <div className="flex flex-col">
         <div className="flex">
           <pre>{JSON.stringify(profile, null, 2)}</pre>
           <div className='ml-auto'><Avatar avatar={profile.avatar} /></div>
         </div>
         <div>
+          <p>Posts: [</p>
           {posts.map(post => (
             <div key={post.id} className="flex">
-              <div><p>Posts: [</p>
-                <pre className="ml-20">{JSON.stringify(post, null, 2)}</pre>
-                <p>]</p>
+              <div>
+                <Link href={`/auth/post/${post.id}`}>
+                  <pre className="ml-20">{JSON.stringify(post, null, 2)}</pre>
+                </Link>
               </div>
             </div>
           ))}
+          <p>]</p>
         </div>
       </div>
     </div>
